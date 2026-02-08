b486e32e2eba1682bc1e098b43a4d58339e9a567
whustafree <magustoregustavo@gmail.com>
2026-02-08 18:57:39 -0300

    as

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'animofree-v42-restored';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => key!==CACHE_NAME && caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch', e => { if(!e.request.url.includes('api')) e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
